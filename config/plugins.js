55de1ceeb8dd4445b33cc6e5e6650f19c06d2e57
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-09-09 20:29:16 +0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,15 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-        secure: true,       
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+        secure: true,
       },
       actionOptions: {
-        upload: { folder: env('CLOUDINARY_FOLDER', 'strapi') },
+        upload: { folder: env("CLOUDINARY_FOLDER", "strapi") },
         delete: {},
       },
     },
