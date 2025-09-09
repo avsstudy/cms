48fdec443fe31665a2f72b5c898b943bb873b0a2
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-09-09 17:34:05 +0300

    update

---
+++ src/api/topic-group/routes/topic-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * topic-group router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::topic-group.topic-group');
