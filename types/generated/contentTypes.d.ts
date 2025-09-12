c299e71d4cd14f7f27d2c01e6209eac7898134db
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-09-12 15:15:00 +0300

    Update contentTypes.d.ts

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -509,6 +509,7 @@ export interface ApiAuthorAuthor extends Struct.CollectionTypeSchema {
   };
   attributes: {
     articles: Schema.Attribute.Relation<'oneToMany', 'api::article.article'>;
+    avatar: Schema.Attribute.Media<'images'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
