e4e4ce43070ea6c0718cc496e37e1a108c94e63f
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-09-24 15:46:57 +0300

    Add spaces

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,35 @@
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "cloudinary",
+//       providerOptions: {
+//         cloud_name: env("CLOUDINARY_NAME"),
+//         api_key: env("CLOUDINARY_KEY"),
+//         api_secret: env("CLOUDINARY_SECRET"),
+//         secure: true,
+//       },
+//       actionOptions: {
+//         upload: { folder: env("CLOUDINARY_FOLDER", "strapi") },
+//         delete: {},
+//       },
+//     },
+//   },
+// });
+// config/plugins.js
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: "strapi-provider-upload-do",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-        secure: true,
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
+        cdn: env("DO_SPACE_CDN"),
       },
       actionOptions: {
-        upload: { folder: env("CLOUDINARY_FOLDER", "strapi") },
+        upload: {},
         delete: {},
       },
     },
