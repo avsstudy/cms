935cda007d737a84f89121b0904b9ac3a032cd33
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-09-30 11:36:37 +0300

    Add ipk

---
+++ src/api/ipk/controllers/ipk.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ipk controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ipk.ipk');
---
+++ src/api/ipk/services/ipk.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ipk service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ipk.ipk');
