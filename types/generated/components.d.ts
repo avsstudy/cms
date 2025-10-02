6455e50bfed68b505adc00d2f115c6810efc3547
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-10-02 13:02:22 +0300

    fix

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -14,16 +14,6 @@ export interface SharedChapter extends Struct.ComponentSchema {
   };
 }
 
-export interface SharedContent extends Struct.ComponentSchema {
-  collectionName: 'components_shared_contents';
-  info: {
-    displayName: 'content';
-  };
-  attributes: {
-    text: Schema.Attribute.Blocks;
-  };
-}
-
 export interface SharedCustomQuote extends Struct.ComponentSchema {
   collectionName: 'components_shared_custom_quotes';
   info: {
@@ -96,71 +86,6 @@ export interface SharedIpk extends Struct.ComponentSchema {
   };
 }
 
-export interface SharedJson extends Struct.ComponentSchema {
-  collectionName: 'components_shared_jsons';
-  info: {
-    displayName: 'json';
-  };
-  attributes: {
-    json: Schema.Attribute.JSON;
-  };
-}
-
-export interface SharedMark extends Struct.ComponentSchema {
-  collectionName: 'components_shared_marks';
-  info: {
-    displayName: 'mark';
-  };
-  attributes: {
-    mark: Schema.Attribute.RichText;
-  };
-}
-
-export interface SharedMedia extends Struct.ComponentSchema {
-  collectionName: 'components_shared_media';
-  info: {
-    displayName: 'Media';
-    icon: 'file-video';
-  };
-  attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
-  };
-}
-
-export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
-  info: {
-    displayName: 'Quote';
-    icon: 'indent';
-  };
-  attributes: {
-    body: Schema.Attribute.Text;
-    title: Schema.Attribute.String;
-  };
-}
-
-export interface SharedRelated extends Struct.ComponentSchema {
-  collectionName: 'components_shared_relateds';
-  info: {
-    displayName: 'related';
-  };
-  attributes: {
-    article: Schema.Attribute.Relation<'oneToOne', 'api::article.article'>;
-  };
-}
-
-export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
-  info: {
-    description: '';
-    displayName: 'Rich text';
-    icon: 'align-justify';
-  };
-  attributes: {
-    body: Schema.Attribute.RichText;
-  };
-}
-
 export interface SharedSeo extends Struct.ComponentSchema {
   collectionName: 'components_shared_seos';
   info: {
@@ -173,18 +98,6 @@ export interface SharedSeo extends Struct.ComponentSchema {
     metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
     metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
     shareImage: Schema.Attribute.Media<'images'>;
-  };
-}
-
-export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
-  info: {
-    description: '';
-    displayName: 'Slider';
-    icon: 'address-book';
-  };
-  attributes: {
-    files: Schema.Attribute.Media<'images', true>;
   };
 }
 
@@ -201,16 +114,6 @@ export interface SharedText extends Struct.ComponentSchema {
         maxLength: 120;
       }>;
     text_content: Schema.Attribute.Blocks;
-  };
-}
-
-export interface SharedVideo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_videos';
-  info: {
-    displayName: 'video';
-  };
-  attributes: {
-    video: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
   };
 }
 
@@ -249,22 +152,13 @@ declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'shared.chapter': SharedChapter;
-      'shared.content': SharedContent;
       'shared.custom-quote': SharedCustomQuote;
       'shared.custom-table': SharedCustomTable;
       'shared.custom-video': SharedCustomVideo;
       'shared.image': SharedImage;
       'shared.ipk': SharedIpk;
-      'shared.json': SharedJson;
-      'shared.mark': SharedMark;
-      'shared.media': SharedMedia;
-      'shared.quote': SharedQuote;
-      'shared.related': SharedRelated;
-      'shared.rich-text': SharedRichText;
       'shared.seo': SharedSeo;
-      'shared.slider': SharedSlider;
       'shared.text': SharedText;
-      'shared.video': SharedVideo;
       'shared.video-review': SharedVideoReview;
       'shared.zakon': SharedZakon;
     }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -422,9 +422,7 @@ export interface ApiAboutAbout extends Struct.SingleTypeSchema {
     draftAndPublish: false;
   };
   attributes: {
-    blocks: Schema.Attribute.DynamicZone<
-      ['shared.media', 'shared.quote', 'shared.rich-text', 'shared.slider']
-    >;
+    blocks: Schema.Attribute.DynamicZone<[]>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
