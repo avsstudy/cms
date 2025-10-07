663b303e931117b34160addab1220d707e303616
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-10-07 13:56:24 +0300

    add

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,3 @@
-// module.exports = ({ env }) => ({
-//   upload: {
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//         cloud_name: env("CLOUDINARY_NAME"),
-//         api_key: env("CLOUDINARY_KEY"),
-//         api_secret: env("CLOUDINARY_SECRET"),
-//         secure: true,
-//       },
-//       actionOptions: {
-//         upload: { folder: env("CLOUDINARY_FOLDER", "strapi") },
-//         delete: {},
-//       },
-//     },
-//   },
-// });
-// config/plugins.js
 module.exports = ({ env }) => ({
   upload: {
     config: {
