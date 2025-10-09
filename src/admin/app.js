73e566ba16eb1b257218c6579c824b41a4535036
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-10-09 11:24:02 +0300

    fix settings

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,18 +1,13 @@
-// /src/admin/app.js
 import ru from "./src/translations/ru.json";
 import uk from "./src/translations/uk.json";
 
 export default {
   config: {
-    // Мови, які з’являться у виборі в профілі адміна:
     locales: ["en", "ru", "uk"],
 
-    // Переклади ключів для цих локалей:
     translations: {
       ru,
       uk,
-      // опційно: кастомізація en
-      // en: { "Auth.form.welcome.title": "Welcome / Привіт" },
     },
   },
   bootstrap(app) {
