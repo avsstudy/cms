11ec796acbcdf59d5babc9371474d0f4e54e14e5
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-11-21 17:09:56 +0200

    add CKEditor

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -729,6 +729,20 @@ export interface ApiCourseCourse extends Struct.CollectionTypeSchema {
       'manyToMany',
       'api::subscription-type.subscription-type'
     >;
+    table: Schema.Attribute.RichText &
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'defaultMarkdown';
+        }
+      >;
+    table_html: Schema.Attribute.RichText &
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'defaultHtml';
+        }
+      >;
     title: Schema.Attribute.String & Schema.Attribute.Required;
     topic: Schema.Attribute.Relation<'manyToMany', 'api::topic.topic'>;
     updatedAt: Schema.Attribute.DateTime;
