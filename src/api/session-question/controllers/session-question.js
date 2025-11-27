a0a72bcda8f34bd235af12dfff4a7fdefa6317f9
Dmytro Stetsenko <229966596+avsstudy@users.noreply.github.com>
2025-11-27 15:53:46 +0200

    add

--- src/api/session-question/controllers/session-question.js
+++ src/api/session-question/controllers/session-question.js
@@ -1,9 +1,45 @@
-'use strict';
+"use strict";
 
-/**
- * session-question controller
- */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController(
+  "api::session-question.session-question",
+  ({ strapi }) => ({
+    async create(ctx) {
+      const user = ctx.state.user;
 
-module.exports = createCoreController('api::session-question.session-question');
+      if (!user) {
+        return ctx.unauthorized("You must be authenticated to ask a question");
+      }
+
+      const { question, sessionId } = ctx.request.body;
+
+      if (!question || !sessionId) {
+        return ctx.badRequest("question and sessionId are required");
+      }
+
+      const session = await strapi.entityService.findOne(
+        "api::study-session.study-session",
+        sessionId
+      );
+
+      if (!session) {
+        return ctx.notFound("Study session not found");
+      }
+
+      const created = await strapi.entityService.create(
+        "api::session-question.session-question",
+        {
+          data: {
+            question,
+            users_permissions_user: user.id,
+            study_session: sessionId,
+          },
+        }
+      );
+
+      const sanitized = await this.sanitizeOutput(created, ctx);
+      return this.transformResponse(sanitized);
+    },
+  })
+);
--- src/api/session-question/routes/session-question.js
+++ src/api/session-question/routes/session-question.js
@@ -1,9 +1,15 @@
-'use strict';
+"use strict";
 
 /**
  * session-question router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::session-question.session-question');
+module.exports = createCoreRouter("api::session-question.session-question", {
+  config: {
+    create: {
+      policies: ["global::is-authenticated"],
+    },
+  },
+});
